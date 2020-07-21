c932b139b3b7fe5bfef564b8e9a0d7deb8672f94
root <root@DESKTOP-V8P4AHJ.localdomain>
2020-07-21 13:17:13 +0200

    simplified dom and factory code

--- factory.js
+++ factory.js
@@ -1,91 +1,14 @@
 function HighlightDay() {
-
-
-    function getttingDayOne(day1) {
-        var date1 = new Date(day1)
-        var weekdays = [{
-            day: "Sunday",
-        },
-        {
-            day: "Monday",
-         },
-        {
-            day: "Tuesday",
-        },
-        {
-            day: "Wednesday",
-        },
-        {
-            day: "Thursday",
-        },
-        {
-            day: "Friday",
-        },
-        {
-            day: "Saturday",
-        }
-        ];
-        for (var i = 0; i < weekdays.length; i++) {
-            console.log(weekdays[date1.getDay()])
-            return weekdays[date1.getDay()];
+    
+    function getDay(day) {
+        var date = new Date(day)
+        var weekDays = [0, 1, 2, 3, 4, 5, 6];
+        for (var i = 0; i < weekDays.length; i++) {
+            console.log(date.getDay());
+            return date.getDay();
         }
     }
-    function getttingDayTwo(day2) {
-        var date2 = new Date(day2)
-        var weekdays = [{
-            day: "Sunday",
-            // style : "color"
-        },
-        {
-            day: "Monday",
-
-        },
-        {
-            day: "Tuesday",
-
-        },
-        {
-            day: "Wednesday",
-
-        },
-        {
-            day: "Thursday",
-
-        },
-        {
-            day: "Friday",
-
-        },
-        {
-            day: "Saturday",
-
-        }
-        ];
-        for (var i = 0; i < weekdays.length; i++) {
-            console.log(weekdays[date2.getDay()])
-            return weekdays[date2.getDay()];
-        }
-    }
-    // function getFirstDay(day1) {
-    //     var date1 = new Date(day1)
-    //     var weekDays = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    //     for (var i = 0; i < weekDays.length; i++) {
-    //         console.log(weekDays[date1.getDay()]);
-    //         return weekDays[date1.getDay()];
-    //     }
-    // }
-    // function getSecondDay(day2) {
-    //     var date2 = new Date(day2);
-    //     var weekDays = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    //     for (var i = 0; i < weekDays.length; i++) {
-    //         console.log(weekDays[date2.getDay()]);
-    //         return weekDays[date2.getDay()];
-    //     }
-    // }
-    return {
-        getttingDayOne,
-        getttingDayTwo
-        // getFirstDay,
-        // getSecondDay,
+   return {
+        getDay
     }
 }
--- tests/test.js
+++ tests/test.js
@@ -1,42 +1,34 @@
 describe("The Matching days exercise", function () {
     describe("the getFirstDay function", function () {
-        it("should return Saturday for date 2020/02/15", function () {
-            var getDay = HighlightDay();
+        it("should return 6 for Saturday with date 2020/02/15", function () {
+            var gettingDay = HighlightDay();
             var day = "2020/02/15";
-            var week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-            assert.deepEqual(getDay.getFirstDay(day, week), "Saturday");
+            assert.deepEqual(gettingDay.getDay(day), 6);
         });
-        it("should return Wednesday for date 2020/04/29", function () {
-            var getDay = HighlightDay();
+        it("should return 3 for Wednesday with date 2020/04/29", function () {
+            var gettingDay = HighlightDay();
             var day = "2020/04/29";
-            var week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-            assert.deepEqual(getDay.getFirstDay(day, week), "Wednesday");
+            assert.deepEqual(gettingDay.getDay(day), 3);
         });
-        it("should return Sunday for date 2020/06/21", function () {
-            var getDay = HighlightDay();
+        it("should return 0 for Sunday with date 2020/06/21", function () {
+            var gettingDay = HighlightDay();
             var day = "2020/06/21";
-            var week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-            assert.deepEqual(getDay.getFirstDay(day, week), "Sunday");
+            assert.deepEqual(gettingDay.getDay(day), 0);
         });
-    });
-    describe("the getSecondDay function", function () {
-        it("should return Monday for date 2017/01/16", function () {
-            var getDay = HighlightDay();
-            var day2 = "2017/01/16";
-            var week2 = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-            assert.deepEqual(getDay.getSecondDay(day2, week2), "Monday");
+        it("should return 1 for Monday with date 2017/01/16", function () {
+            var gettingDay = HighlightDay();
+            var day = "2017/01/16";
+            assert.deepEqual(gettingDay.getDay(day), 1);
         });
-        it("should return Friday for date 2017/11/24", function () {
-            var getDay = HighlightDay();
-            var day2 = "2017/11/24";
-            var week2 = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-            assert.deepEqual(getDay.getSecondDay(day2, week2), "Friday");
+        it("should return 5 for Friday with date 2017/11/24", function () {
+            var gettingDay = HighlightDay();
+            var day = "2017/11/24";
+            assert.deepEqual(gettingDay.getDay(day), 5);
         });
-        it("should return Thursday for date 2017/09/21", function () {
-            var getDay = HighlightDay();
-            var day2 = "2017/09/21";
-            var week2 = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-            assert.deepEqual(getDay.getSecondDay(day2, week2), "Thursday");
+        it("should return 4 for Thursday with date 2017/09/21", function () {
+            var gettingDay = HighlightDay();
+            var day = "2017/09/21";
+            assert.deepEqual(gettingDay.getDay(day), 4);
         });
     });
 });

0223059cf38eecd780a23e9cff5bce209c967cfc
root <root@DESKTOP-V8P4AHJ.localdomain>
2020-07-22 17:30:05 +0200

    changed setTimeout to forEach

--- match.js
+++ match.js
@@ -14,19 +14,17 @@ function myFunction() {
     var weekday = [sunday, monday, tuesday, wednesday, thursday, friday, saturday]
     var dateOne = toGetDay.getDay(date1.value);
     var dateTwo = toGetDay.getDay(date2.value);
-    if (weekday[dateOne] === weekday[dateTwo]) {
+    weekday.forEach((dayElem) => {
+        dayElem.classList.remove("same");
+        dayElem.classList.remove("col1");
+        dayElem.classList.remove("col2");
+    });
+    if(weekday[dateOne] === weekday[dateTwo]) {
         weekday[dateOne].classList.add("same")
     } else {
         weekday[dateOne].classList.add("col1")
         weekday[dateTwo].classList.add("col2")
-    }
-
-    setTimeout(function () {
-        weekday[dateOne].classList.remove("same");
-        weekday[dateOne].classList.remove("col1");
-        weekday[dateTwo].classList.remove("col2")
-    }, 4000);
+    } 
 };
-
 date1.addEventListener("change", myFunction);
 date2.addEventListener("change", myFunction);
